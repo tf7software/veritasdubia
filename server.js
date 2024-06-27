6c6832af0fc42cb9abacb10fd67cd30faf204e15
Henry Warren <contact@twentyfour7.xyz>
2024-06-27 17:55:12 -0400

    server port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { GoogleGenerativeAI } = require('@google/generative-ai');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
